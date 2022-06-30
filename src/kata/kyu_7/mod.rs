3b01f7f0c8119090d65416eb8cb533b7c1bd31e8
Sejden <sejden@outlook.com>
2022-06-30 01:25:51 -0500

    Add k7: gimme

--- src/kata/kyu_7/mod.rs
+++ src/kata/kyu_7/mod.rs
@@ -1,1 +1,2 @@
 pub mod maskify;
+pub mod gimme;
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 #![allow(unused)]
 
 use code_wars::kata::{kyu_6, kyu_7};
+use rand;
+use std::time::Instant;
 
 fn main() {
-    println!(
-        "{:?}",
-        kyu_7::maskify::maskify("Nananana Batman!")
-    );
+    let rounds = 100;
+    let tries = 10_000_000;
+    let mut time = 0.0;
+    
+    for round in 0..rounds {
+        let now = Instant::now();
+        let mut value = rand::random::<[i32;3]>();
+        for _ in 0..tries {
+            kyu_7::gimme::gimme(value);
+            value = rand::random();
+        }
+        
+        time += now.elapsed().as_secs_f64();
+    }
+    
+    println!("Average time: {:.4}s", time / rounds as f64);
 }
