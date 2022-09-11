d160dd74e215c683387472f06c9a2e7bc0518a08
Behilam <praehil@gmail.com>
2022-09-11 14:26:49 -0500

    Refactor

--- src/kata/kyu_5/human_readable_time.rs
+++ src/kata/kyu_5/human_readable_time.rs
@@ -12,8 +12,7 @@
 
 pub fn make_readable(seconds: u32) -> String {
     let hours = seconds / 3600;
-    let seconds = seconds % 3600;
-    let minutes = seconds / 60;
+    let minutes = seconds % 3600 / 60;
     let seconds = seconds % 60;
     format!("{:02}:{:02}:{:02}", hours, minutes, seconds)
 }
